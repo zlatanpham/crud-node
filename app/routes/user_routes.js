4213cabf34dca4e5410e2886fd8d84870ba53e9b
zlatan <zlatan.pham@gmail.com>
2019-02-28 18:27:44 +0700

    GET /users & /users/:id

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,32 +1,33 @@
 const yup = require('yup');
+const ObjectId = require('mongodb').ObjectId;
 
 module.exports = function(app, db) {
-  // app.get('/notes', (req, res) => {
-  //   const result = db
-  //     .collection('notes')
-  //     .find({})
-  //     .toArray((err, result) => {
-  //       if (err) {
-  //         res.send(err);
-  //       } else {
-  //         res.send(result);
-  //       }
-  //     });
-  // });
+  app.get('/users', (req, res) => {
+    const result = db
+      .collection('users')
+      .find({})
+      .toArray((err, result) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(result);
+        }
+      });
+  });
 
-  // app.get('/notes/:id', (req, res) => {
-  //   const { id } = req.params;
-  //   const details = { _id: id };
-  //   console.log(details);
-  //   db.collection('notes').findOne(details, (err, item) => {
-  //     console.log(item);
-  //     if (err) {
-  //       res.send({ error: 'An error has occurred' });
-  //     } else {
-  //       res.send(item);
-  //     }
-  //   });
-  // });
+  app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const details = { _id: id };
+    console.log(details);
+    db.collection('users').findOne(ObjectId(id), (err, item) => {
+      console.log(item);
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
 
   app.post('/users', (req, res) => {
     const schema = yup.object().shape({
@@ -34,6 +35,8 @@ module.exports = function(app, db) {
       lastName: yup.string().required(),
     });
     schema.isValid(req.body).then(valid => {
+      console.log(req.body);
+      console.log(valid);
       if (valid) {
         db.collection('users').insertOne(
           { ...req.body, createdOn: new Date() },
