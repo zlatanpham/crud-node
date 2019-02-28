66b1672884084aba7d69114428565896730c81e3
zlatan <zlatan.pham@gmail.com>
2019-02-28 13:34:22 +0700

    connect to db

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -1,6 +1,41 @@
 module.exports = function(app, db) {
+  app.get('/notes', (req, res) => {
+    const result = db
+      .collection('notes')
+      .find({})
+      .toArray((err, result) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(result);
+        }
+      });
+  });
+
+  app.get('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const details = { _id: id };
+    console.log(details);
+    db.collection('notes').findOne(details, (err, item) => {
+      console.log(item);
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
     // You'll create your note here.
-    res.send('Hello');
+    console.log(req.body);
+    db.collection('notes').insertOne({ test: 'test' }, (err, result) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        console.log('success');
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const db = require('./config/db');
+dotenv.config();
+
 const app = express();
 
+console.log(`Your port is ${process.env.PORT}`); // 8626
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = 8000;
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err);
+
+  // Make sure you add the database name and not the collection name
+  dtb = database.db('local');
+  require('./app/routes')(app, dtb);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
 });
