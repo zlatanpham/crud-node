9c03c2e290c58b525b7a41ff840f046f805c9928
zlatan <zlatan.pham@gmail.com>
2019-03-05 16:57:31 +0700

    update delete api

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -12,103 +12,58 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.json(user);
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findByIdAndDelete(id, err => {
+    if (err) next(err);
+    console.log(id);
+    res.send('ok');
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const schema = yup.object().shape({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    address: yup.string().required(),
+  });
+  schema.isValid(req.body).then(valid => {
+    if (valid) {
+      User.create({ ...req.body, createdAt: new Date() }, (err, result) => {
+        if (err) next(err);
+        res.json(result);
+      });
+    } else {
+      // next(err);
+      res.status(400).send({ err: 'err' });
+    }
+  });
+});
+
+router.post('/deletes', (req, res) => {
+  const schema = yup.object().shape({
+    ids: yup.array(yup.string()).required(),
+  });
+
+  schema.isValid(req.body).then(valid => {
+    if (valid) {
+      User.deleteMany({ _id: { $in: req.body.ids } }, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+    } else {
+      // next(err);
+      res.status(400).send({ err: 'err' });
+    }
+  });
+});
+
 module.exports = router;
-
-// module.exports = function(app) {
-//   // app.get('/users', (req, res) => {
-//   //   const result = db
-//   //     .collection('users')
-//   //     .find({})
-//   //     .toArray((err, result) => {
-//   //       if (err) {
-//   //         res.send(err);
-//   //       } else {
-//   //         res.send(result);
-//   //       }
-//   //     });
-//   // });
-
-//   // app.get('/users', (req, res) => {
-//   //   const result = db
-//   //     .collection('users')
-//   //     .find({})
-//   //     .toArray((err, result) => {
-//   //       if (err) {
-//   //         res.send(err);
-//   //       } else {
-//   //         res.send(result);
-//   //       }
-//   //     });
-//   // });
-
-//   // app.get('/users/:id', (req, res) => {
-//   //   const { id } = req.params;
-//   //   const details = { _id: id };
-//   //   console.log(details);
-//   //   db.collection('users').findOne(ObjectId(id), (err, item) => {
-//   //     console.log(item);
-//   //     if (err) {
-//   //       res.send({ error: 'An error has occurred' });
-//   //     } else {
-//   //       res.send(item);
-//   //     }
-//   //   });
-//   // });
-
-//   // app.delete('/users/:id', (req, res) => {
-//   //   const { id } = req.params;
-//   //   try {
-//   //     db.collection('users').deleteOne({ _id: ObjectId(id) }, (err, obj) => {
-//   //       if (err) throw err;
-//   //       res.send('ok');
-//   //     });
-//   //   } catch (e) {
-//   //     throw e;
-//   //   }
-//   // });
-
-//   // app.delete('/users', (req, res) => {
-//   //   console.log('delete');
-
-//   //   const schema = yup.object().shape({
-//   //     ids: yup.array(yup.string()).required(),
-//   //   });
-
-//   //   schema.isValid(req.body).then(valid => {
-//   //     try {
-//   //       const details = { _id: { $in: req.body.ids.map(id => ObjectId(id)) } };
-//   //       db.collection('users').deleteMany(details, (err, obj) => {
-//   //         if (err) throw err;
-//   //         res.send('ok');
-//   //       });
-//   //     } catch (e) {
-//   //       throw e;
-//   //     }
-//   //   });
-//   // });
-
-//   // app.post('/users', (req, res) => {
-//   //   const schema = yup.object().shape({
-//   //     firstName: yup.string().required(),
-//   //     lastName: yup.string().required(),
-//   //     address: yup.string().required(),
-//   //   });
-//   //   schema.isValid(req.body).then(valid => {
-//   //     if (valid) {
-//   //       db.collection('users').insertOne(
-//   //         { ...req.body, createdOn: new Date() },
-//   //         (err, result) => {
-//   //           if (err) {
-//   //             res.send({ error: 'An error has occurred' });
-//   //           } else {
-//   //             res.send(result.ops[0]);
-//   //           }
-//   //         },
-//   //       );
-//   //     } else {
-//   //       console.log('error 400');
-//   //       res.status(400).send({ err: 'errro' });
-//   //     }
-//   //   });
-//   // });
-// };
